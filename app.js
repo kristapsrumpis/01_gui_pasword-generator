c48d45c4e7838620ebd7f6f01f12ea2023647b07
Kristaps Rumpis <43776913+kristapsrumpis@users.noreply.github.com>
2024-03-31 10:42:40 +0100

    first init

---
+++ app.js
@@ -0,0 +1,37 @@
+const { app,  BrowserWindow} = require('electron/main')
+
+
+const createWindow = function(){
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+          }
+    })
+
+    win.loadFile('src/templates/index.html')
+    win.on('closed', ()=>{
+        win.quite
+    })
+}
+
+app.whenReady().then(()=>{
+    createWindow()
+
+    app.on('activate', ()=> {
+        if(BrowserWindow.getAllWindows().length === 0){
+            createWindow()
+        }
+    })
+})
+
+
+
+app.on('window-all-closed', ()=>{
+    if(process.platform !== 'darvin'){
+        app.quite
+    }
+})

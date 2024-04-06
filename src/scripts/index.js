7d92110c9f6a64887bd5b963743e6ecd8a657924
Kristaps Rumpis <43776913+kristapsrumpis@users.noreply.github.com>
2024-04-06 20:32:27 +0100

    add gui check box to select options

---
+++ src/scripts/index.js
@@ -0,0 +1,13 @@
+
+
+// abcdefghijklmnopqrstuvwxyz
+// ABCDEFGHIJKLMNOPQRSTUVWXYZ
+// 0123456789
+// !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+
+
+const lettersLowercase = 'abcdefghijklmnopqrstuvwxyz'
+const lettersUppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const numbers = '0123456789'
+const symbols = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+

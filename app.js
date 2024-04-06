b55c72adb04f1506f130ef05ad4a9a3b82aa87de
Kristaps Rumpis <43776913+kristapsrumpis@users.noreply.github.com>
2024-04-06 16:09:44 +0100

    add header

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const createWindow = function(){
     const win = new BrowserWindow({
         width: 800,
         height: 600,
-        autoHideMenuBar: true,
+        // autoHideMenuBar: true,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,

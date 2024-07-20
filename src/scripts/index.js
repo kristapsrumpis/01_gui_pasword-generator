7df42829c945f4b0cf147a99eccc96e5887cd0fc
Kristaps Rumpis <43776913+kristapsrumpis@users.noreply.github.com>
2024-07-20 17:50:34 +0100

    expand javascript functionality and add cople tweeks to html and css

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -11,3 +11,26 @@ const lettersUppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 const numbers = '0123456789'
 const symbols = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
+
+// select generate BTN 
+const generateBtn = document.querySelector('#generateBtn')
+
+// set defoults 
+document.querySelector('#lethersLowercase').checked = true
+document.querySelector('#lethersUppercase').checked = false
+document.querySelector('#numbers').checked = false
+document.querySelector('#symbols').checked = false
+
+generateBtn.addEventListener('click', function(){
+    let lethersLowercase = document.querySelector('#lethersLowercase').checked
+    let lettersUppercase = document.querySelector('#lethersUppercase').checked
+    let numbers = document.querySelector('#numbers').checked
+    let symbols = document.querySelector('#symbols').checked
+    let passwordLength = document.querySelector('#passwordLength').value
+    let notToUse = document.querySelector('#notToUse').value
+
+    console.log(lethersLowercase, lettersUppercase, numbers, symbols)
+    console.log(+passwordLength, notToUse)
+})
+
+

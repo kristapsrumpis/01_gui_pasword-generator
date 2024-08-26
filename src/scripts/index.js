f817632eb989eb19e32bc13c9c78c40f28da54dc
Kristaps Rumpis <43776913+kristapsrumpis@users.noreply.github.com>
2024-08-26 14:46:58 +0100

    basic finish next steps optimization falow

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 
-
+// simbol lists for passwords
+//
 // abcdefghijklmnopqrstuvwxyz
 // ABCDEFGHIJKLMNOPQRSTUVWXYZ
 // 0123456789
@@ -15,22 +16,87 @@ const symbols = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 // select generate BTN 
 const generateBtn = document.querySelector('#generateBtn')
 
+
 // set defoults 
 document.querySelector('#lethersLowercase').checked = true
 document.querySelector('#lethersUppercase').checked = false
 document.querySelector('#numbers').checked = false
 document.querySelector('#symbols').checked = false
 
+
+// returns list of symbols, letahers, numbers for password depeds from selection on input
+const passwordSymbolList = function(Low,Up,num,symb){
+    let characters = ''
+    if(Low == true){
+        characters += lettersLowercase
+    }
+    if(Up == true){
+        characters += lettersUppercase
+    }
+    if(num == true){
+        characters += numbers
+    }
+    if(symb == true){
+        characters += symbols
+    }
+    return characters
+} 
+
+// add's event listgener functionaity to generate BTN
 generateBtn.addEventListener('click', function(){
-    let lethersLowercase = document.querySelector('#lethersLowercase').checked
-    let lettersUppercase = document.querySelector('#lethersUppercase').checked
-    let numbers = document.querySelector('#numbers').checked
-    let symbols = document.querySelector('#symbols').checked
+    let password = ''
+
+    let lethersLowercaseBoolean = document.querySelector('#lethersLowercase').checked
+    let lettersUppercaseBoolean = document.querySelector('#lethersUppercase').checked
+    let numbersBoolean = document.querySelector('#numbers').checked
+    let symbolsBoolean = document.querySelector('#symbols').checked
     let passwordLength = document.querySelector('#passwordLength').value
     let notToUse = document.querySelector('#notToUse').value
 
-    console.log(lethersLowercase, lettersUppercase, numbers, symbols)
-    console.log(+passwordLength, notToUse)
+
+    let characters = passwordSymbolList(lethersLowercaseBoolean,lettersUppercaseBoolean,numbersBoolean,symbolsBoolean)
+
+    if(!notToUse == ''){
+        let newCharacters = characters
+
+        console.log(notToUse, notToUse.length)
+        console.log(characters, characters.length)
+
+        for(let i = 0; i< characters.length; i++){
+            if(characters[i]){
+                for(let e = 0; e < notToUse.length; e++){
+                    if(notToUse[e]){
+                        if(characters[i] === notToUse[e]){
+                            newCharacters = newCharacters.replace(notToUse[e], '')
+                        }
+                    }
+                }
+            }
+        }
+        console.log(newCharacters, newCharacters.length)
+        characters = newCharacters
+    }
+
+
+
+    for(i = 0; i< passwordLength; i++){
+
+        // generates random number
+        let randomNumber = Math.floor(Math.random() * 100)
+
+        // cheks random number if it fits under selected character length 
+        // if random number is highter then selected character length then set back counter and generate new random number
+        if(randomNumber < characters.length){
+                //add's to password str
+                password += characters[randomNumber]      
+        }else{
+            i--
+        }
+    }
+
+     //get password field and print generated pasword
+    let passwordOutput = document.querySelector('#password')
+    passwordOutput.innerHTML = password
 })
 
 
